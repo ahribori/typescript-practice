9ba2680fb4831b33c3e9d13f6bc1a1985e8a5f1b
ahribori <ahribori@gmail.com>
2018-04-25 00:33:42 +0900

    인터페이스2

--- 002_interface.ts
+++ 002_interface.ts
@@ -64,3 +64,91 @@ let ro: ReadonlyArray<number> = a;
 // ro.push(5); // 오류!
 // ro.length = 100; // 오류!
 // a = ro; // 오류!
+
+//-----------------------------
+
+/**
+ * 타입 초과검사 회피 (권장하지 않음)
+ */
+interface SquareConfig2 {
+    color?: string;
+    width?: number;
+    [propName: string]: any;
+}
+
+let squareOptions = { c: 'red', width: 100 };
+let mySquare2 = createSquare(squareOptions);
+
+//-----------------------------
+
+/**
+ * 함수 타입
+ */
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+
+mySearch = (source: string, subString: string) => {
+    let result = source.search(subString);
+    return result > -1;
+};
+
+mySearch = (source: string, subString: string) => {
+    let result = source.search(subString);
+    return result > -1;
+};
+
+mySearch = (src: string, sub: string) => { // 변수명은 일치하지 않아도 된다
+    let result = src.search(sub);
+    return result > -1;
+};
+
+//-----------------------------
+
+/**
+ * 인덱싱 타입
+ */
+interface StringArray {
+    [index: number]: string;
+}
+
+let myArray: StringArray;
+
+myArray = ['Ahri', 'Bori'];
+
+myArray = {
+  0: 'a',
+  1: 'b',
+};
+
+// myArray = {
+//     'a': 1, // 오류
+//     'b': 2,
+// };
+
+class Animal {
+    name: string;
+}
+class Dog extends Animal {
+    breed: string;
+}
+
+// 오류: numeric과 string으로 인덱싱하면 완전히 다른 타입의 Animal을 얻을 수 있습니다!
+interface NotOkay {
+    // [x: number]: Animal;
+    [x: string]: Dog;
+}
+
+interface NumberDictionary {
+    [index: string]: number;
+    length: number; // 좋아요, length는 number입니다.
+    // name: string; // 오류, 'name'의 타입이 인덱서의 하위 타입이 아닙니다.
+}
+
+interface ReadonlyStringArray {
+    readonly [index: number]: string;
+}
+let myArr: ReadonlyStringArray = ["Alice", "Bob"];
+// myArr[2] = "Mallory"; // 오류!
