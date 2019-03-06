a379ee51c0ae5ffce1d892e6ef8eee0d105a9abd
ahribori <ahribori@gmail.com>
2019-03-07 00:50:43 +0900

    Generic 학습

--- practice/generic.ts
+++ practice/generic.ts
@@ -1,3 +1,7 @@
+/**
+ * Basic generic
+ */
+
 class Printer<T> {
     constructor() {
     }
@@ -19,5 +23,49 @@ const nestedTypePrinter = new Printer<Printer<number>>();
 
 numberPrinter.print(13);
 stringPrinter.print('안녕!');
-personPrinter.print({name: '정현승', age: 3});
+personPrinter.print({ name: '정현승', age: 3 });
 nestedTypePrinter.print(numberPrinter);
+
+
+class MyGenericClass<T> {
+    private _name: T;
+
+    constructor(name: T) {
+        this._name = name;
+    }
+}
+
+const myClass: MyGenericClass<string> = new MyGenericClass('Daniel');
+
+/**
+ * Generic with extends
+ */
+
+// T를 제한할 수 있다.
+class MyClassWithExtendGeneric<T extends string | number> {
+    private _name: T;
+
+    constructor(name: T) {
+        this._name = name;
+    }
+}
+
+const myClassWithExtendsGeneric = new MyClassWithExtendGeneric(3);
+
+
+/**
+ * Generic with multiple types
+ */
+
+class MyClassWithMultipleGeneric<T, K> {
+    private _name: T;
+    private _age: K;
+
+    constructor(name: T, age: K) {
+        this._name = name;
+        this._age = age;
+    }
+}
+
+new MyClassWithMultipleGeneric<string, number>('야', 32);
+
