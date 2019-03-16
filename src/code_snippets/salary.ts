c61089b487a94cb241d62453916c77e9f5d09f77
ahribori <ahribori@gmail.com>
2019-03-17 01:23:09 +0900

    월 급여 1000만원 미만에 대한 소득구간 계산

--- src/code_snippets/salary.ts
+++ src/code_snippets/salary.ts
@@ -11,13 +11,30 @@ enum SalaryType {
 class MySalary implements ISalaryInterface {
   annualSalary: number;
   salary: number;
+  rangeSalary: number;
+  rangeAnnualSalary: number;
 
   constructor(salaryType: SalaryType, value: number) {
     this.annualSalary = salaryType === SalaryType.MONTH ? value * 12 : value;
     this.salary = Math.round(this.annualSalary / 12);
+    this.rangeSalary = this.calculateRangeSalary();
+    this.rangeAnnualSalary = this.rangeSalary * 12;
+  }
+
+  private calculateRangeSalary(): number {
+    if (this.salary < 1500000) {
+      return this.salary - (this.salary % 5000) + 2500;
+    } else if (this.salary < 3000000) {
+      return this.salary - (this.salary % 10000) + 5000;
+    } else if (this.salary < 10000000) {
+      return this.salary - (this.salary % 20000) + 10000;
+    } else {
+      // TODO
+    }
+    return 0;
   }
 }
 
-const salary = new MySalary(SalaryType.MONTH, 3200000);
+const salary = new MySalary(SalaryType.MONTH, 2503000);
 
 console.log(salary);
